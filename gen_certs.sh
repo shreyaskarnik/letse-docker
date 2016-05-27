ebfca6684769780d095a04f7ef0dd36942d80e16
Shreyas Karnik <karnik.shreyas@gmail.com>
2016-05-26 23:30:31 -0500

    bugfix

--- gen_certs.sh
+++ gen_certs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "$1" = 'new' ]; then
+if [ "$1" == 'new' ]; then
   echo "Generating New LetsEncrypt Certificate for $2 at /etc/certs based on DNS challenge"
   cd /etc/certs && \
   letse keygen -k rsa -b 2048 && \
@@ -12,9 +12,10 @@ if [ "$1" = 'new' ]; then
   echo "Generating Full Chain Certificate" && \
   curl https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem -o lets-encrypt-x3-cross-signed.pem && \
   cat $2.crt lets-encrypt-x3-cross-signed.pem > $2-fullchain.pem
+  rm lets-encrypt-x3-cross-signed.pem  private_key.pem  public_key.pem
 fi
 
-if ["$1" = 'renew']; then
+if ["$1" == 'renew']; then
   echo "Renewing LetsEncrypt Certificate for $2 at /etc/certs/$2.crt"
   cd /etc/certs && \
   letse renew $1.crt -f
